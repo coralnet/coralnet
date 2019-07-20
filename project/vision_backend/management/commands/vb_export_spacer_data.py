09b3dc9da62a188e886b3c54d0ec76844d9d2962
Oscar Beijbom <oscar.beijbom@gmail.com>
2019-07-20 15:03:07 -0700

    Clean-up of current vision backend management scripts. Mostly PEP8 stuff

--- project/vision_backend/management/commands/vb_export_spacer_data.py
+++ project/vision_backend/management/commands/vb_export_spacer_data.py
@@ -19,21 +19,23 @@ class Command(BaseCommand):
         parser.add_argument('--bucket', type=str, help="bucket name to export to")
 
     def handle(self, *args, **options):
+        # TODO: Update this code in NEXT PR
+        raise NotImplementedError('THIS MANAGEMENT TOOL IS NOT COMPLETE NOR TESTED')
 
-        source = Source.objects.get(id=options['source_id'])
-        images = Image.objects.filter(source=source)[:options['nbr_images']]
-        print('Copying {} images from {} to {}'.format(len(images), settings.AWS_STORAGE_BUCKET_NAME,
-                                                       options['bucket']))
-        c = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
-        dst = c.get_bucket(options['bucket'])
-        for img in images:
-
-            full_image_path = posixpath.join(settings.AWS_LOCATION, img.original_file.name)
-            from_name = settings.FEATURE_VECTOR_FILE_PATTERN.format(full_image_path=full_image_path)
-            to_name = img.metadata.name + '.features.json'
-            dst.copy_key(to_name, settings.AWS_STORAGE_BUCKET_NAME, from_name)
-
-            rowcols = [[point.row, point.column] for point in Point.objects.filter(image=img).order_by('id')]
-            k = Key(dst)
-            k.key = img.metadata.name + '.rowcol.json'
-            k.set_contents_from_string(json.dumps(rowcols))
+        # source = Source.objects.get(id=options['source_id'])
+        # images = Image.objects.filter(source=source)[:options['nbr_images']]
+        # print('Copying {} images from {} to {}'.format(len(images), settings.AWS_STORAGE_BUCKET_NAME,
+        #                                                options['bucket']))
+        # c = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+        # dst = c.get_bucket(options['bucket'])
+        # for img in images:
+        #
+        #     full_image_path = posixpath.join(settings.AWS_LOCATION, img.original_file.name)
+        #     from_name = settings.FEATURE_VECTOR_FILE_PATTERN.format(full_image_path=full_image_path)
+        #     to_name = img.metadata.name + '.features.json'
+        #     dst.copy_key(to_name, settings.AWS_STORAGE_BUCKET_NAME, from_name)
+        #
+        #     rowcols = [[point.row, point.column] for point in Point.objects.filter(image=img).order_by('id')]
+        #     k = Key(dst)
+        #     k.key = img.metadata.name + '.rowcol.json'
+        #     k.set_contents_from_string(json.dumps(rowcols))
