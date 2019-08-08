d9b44343109f79dce4aa29fc571863ed356a2b08
Oscar Beijbom <oscar.beijbom@gmail.com>
2019-08-07 22:32:34 -0700

    working version

--- project/labels/views.py
+++ project/labels/views.py
@@ -33,9 +33,19 @@ from .models import Label, LocalLabel, LabelSet
 from .utils import search_labels_by_text, is_label_editable_by_user
 
 
+@login_required
 def duplicates_overview(request):
+    """
+    Renders the view for the duplicates overview.
+    """
+
+    dups = Label.objects.exclude(duplicate=None)
     return render(request, 'labels/list_duplicates.html', {
-        'tmp': 'hello'
+        'labels': dups,
+        'stats': {
+            'ann_count': sum([dup.ann_count for dup in dups]),
+            'dup_count': len(dups),
+        }
     })
 
 @login_required
