6d9aa4f5b5225f16e8d9b235b5ae72e434e2e247
Oscar Beijbom <oscar.beijbom@gmail.com>
2019-09-12 00:02:52 -0700

    added permission decorator

--- project/newsfeed/views.py
+++ project/newsfeed/views.py
@@ -3,12 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+from lib.decorators import news_item_permission_required
 
 from .models import NewsItem, NewsSubItem
+from images.models import Source
+
 
 @permission_required('is_superuser')
 def global_feed(request):
-    print('in main')
     return render(request, 'newsfeed/global.html', {
         'news_items':
             [item.render_view() for item in
@@ -16,8 +18,8 @@ def global_feed(request):
     })
 
 
+@news_item_permission_required('news_item_id', perm=Source.PermTypes.EDIT.code)
 def one_event(request, news_item_id):
-    print('in one')
     return render(request, 'newsfeed/details.html', {
         'main': NewsItem.objects.get(id=news_item_id).render_view(),
         'subs': [item.render_view() for item in
