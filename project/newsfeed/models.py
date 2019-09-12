fc775a746b9f03ade4eb3e9cfc83171194b675bb
Oscar Beijbom <oscar.beijbom@gmail.com>
2019-09-12 00:13:48 -0700

    added user_id and user_username fields

--- project/newsfeed/models.py
+++ project/newsfeed/models.py
@@ -10,9 +10,12 @@ from images.models import Source
 # Create your models here.
 
 
-def log_item(source, app, message):
+def log_item(source, user, app, message):
+    """ Convenience method for creating a new NewsItem. """
     ns = NewsItem(source_name=source.name,
                   source_id=source.id,
+                  user_id=user.id,
+                  user_username=user.username,
                   message=message,
                   app=app)
     ns.save()
@@ -20,6 +23,7 @@ def log_item(source, app, message):
 
 
 def log_sub_item(news_item, message):
+    """ Convenience method for creating a new NewsSubItem. """
     ni = NewsSubItem(news_item=news_item, message=message)
     ni.save()
 
@@ -29,9 +33,14 @@ class NewsItem(models.Model):
     as well on an aggregate listings. """
 
     # Not using foreign keys since we don't want to delete a news-item if a
-    # source is deleted.
+    # source or user is deleted.
     source_id = models.IntegerField(null=False, blank=False)
     source_name = models.CharField(null=False, blank=False, max_length=200)
+
+    # Don't require the user to be set.
+    user_id = models.IntegerField(null=False, blank=False)
+    user_username = models.CharField(null=False, blank=False, max_length=50)
+
     message = models.TextField(null=False, blank=False, max_length=500)
     app = models.CharField(null=False, blank=False, max_length=50,
                            choices=[(a, b) for a, b in
@@ -41,10 +50,13 @@ class NewsItem(models.Model):
     datetime = models.DateTimeField(auto_now_add=True, editable=False)
 
     def render_view(self):
+        """ Renders the record into a dictionary used in the views. """
 
         curated = {
             'source_name': self.source_name,
             'source_id': self.source_id,
+            'user_username': self.user_username,
+            'user_id': self.user_id,
             'app': self.app,
             'message': self.message.format(subcount=NewsSubItem.objects.
                                            filter(news_item=self).count()),
@@ -74,6 +86,7 @@ class NewsSubItem(models.Model):
     datetime = models.DateTimeField(auto_now_add=True, editable=False)
 
     def render_view(self):
+        """ Renders the record into a dictionary used in the views. """
 
         return {
             'message': self.message,
