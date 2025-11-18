3a61279e69bb55acb226addd2007907e51e8c37a
StephenChan <stephenjchan@gmail.com>
2025-11-17 19:31:06 -0800

    Per django-ses issue 258, AWS_SES_REGION_ENDPOINT is either not needed or may be breaking things.

--- project/config/settings.py
+++ project/config/settings.py
@@ -239,7 +239,6 @@ else:
 
 # [django-ses settings]
 AWS_SES_REGION_NAME = env('AWS_SES_REGION_NAME', default='us-west-2')
-AWS_SES_REGION_ENDPOINT = f'email.{AWS_SES_REGION_NAME}.amazonaws.com'
 # We don't have backward-compat obligations with v1, so we use v2.
 # https://aws.amazon.com/blogs/messaging-and-targeting/upgrade-your-email-tech-stack-with-amazon-sesv2-api/
 USE_SES_V2 = True
